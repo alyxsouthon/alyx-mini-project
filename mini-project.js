d4d37f9a017c9b2bc8964e1826e61930e240c10f
“alyxsouthon” <alyxleo@gmail.com>
2026-01-15 09:21:50 +0000

    adding mini project files to my own repository

---
+++ mini-project.js
@@ -0,0 +1,29 @@
+console.log("Hello!")
+
+let number = 0
+
+//let currentPlayer = "X"
+//let gameOver = false
+
+//function checkWinner
+
+// function to make a move. The player decides which square they want to press. 
+// assign each button an index on the array?
+function makeMove(row, col){
+
+}
+
+// function to handle the click. What happens once the player chooses a square?
+// If the player is X, put X in the box. If the player is O, put O in the box. 
+// This needs to call on makeMove?
+function handleClick(row, col){
+
+    number++
+    console.log(number)
+}
+
+let board = [
+    [" ", " ", " "], 
+    [" ", " ", " "], 
+    [" ", " ", " "]
+]
